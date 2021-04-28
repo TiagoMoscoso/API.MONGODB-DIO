bbfa0dd5abdd8eef607ccb549a673e0976c038e8
TiagoMoscoso <tiagocarvalhomoscoso@gmail.com>
2021-04-28 18:59:50 -0300

    Primeiro commit

---
+++ Api/Data/Collections/Infectados.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MongoDB.Driver.GeoJsonObjectModel;
+public class Infectados
+{
+    public Infectados(DateTime dataNascimento, string sexo, double latitude, double longitude)
+    {
+        this.DataNascimento = dataNascimento;
+        this.Sexo = sexo;
+        this.Localizacao = new GeoJson2DGeographicCoordinates(longitude, latitude);
+    }
+
+    public DateTime DataNascimento { get; set; }
+    public string Sexo { get; set; }
+    public GeoJson2DGeographicCoordinates Localizacao { get; set; }
+}
+}
